966933b6bebaeab39263be28ccf160be702b5191
klapdev <114496790+klapdev@users.noreply.github.com>
2023-03-09 03:59:38 +0800

    added wsp001 ex1, ex2

--- WSP001/ex1/app.ts
+++ WSP001/ex1/app.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 import fs from "fs";
 
 import Cls from "./Cls";
---
+++ WSP001/ex2/1.ts
@@ -0,0 +1,111 @@
+// 1. The following findFactors function.
+function findFactors(num: number) {
+    let factors: number[] = [];
+    for (let factor: number = 2; factor <= num / 2; factor++) {
+        if (num % factor === 0) {
+            factors.push(factor);
+        }
+    }
+    return factors;
+}
+
+console.log(`// 1. Find Factor Function: \n`, findFactors(500));
+
+
+// 2. This LeapYear function
+function leapYear(year: number): boolean {
+    if (year % 400 === 0) {
+        console.log("Leap Year");
+        return true;
+    } else if (year % 100 === 0) {
+        console.log("Not a Leap Year");
+        return false;
+    } else if (year % 4 === 0) {
+        console.log("Leap Year");
+        return true;
+    } else {
+        console.log("Not a Leap Year");
+        return false;
+    }
+}
+
+console.log(`// 2. Weather Leap Year Function: \n`, leapYear(2000));
+
+// 3. This RNATranscription function
+function rnaTranscription(dna: string): string {
+    let rna: string = "";
+    for (let nucleotide of dna) {
+        switch (nucleotide) {
+            case "G":
+                rna += "C";
+                break;
+            case "C":
+                rna += "G";
+                break;
+            case "T":
+                rna += "A";
+                break;
+            case "A":
+                rna += "U";
+                break;
+            default:
+                throw new Error(`The nucleotide ${nucleotide} does not exist`);
+        }
+    }
+    return rna;
+}
+
+console.log(`// 3. RNA Transcription Function: \n`, rnaTranscription("GCTA"))
+
+// 4. The factorial recursive function.
+function factorial(number: number): number {
+    if (number === 0 || number === 1) {
+        return 1;
+    }
+
+    return factorial(number - 1) * number;
+}
+
+console.log(`// 4. The factorial recursive function: \n`, factorial(10));
+
+
+// 5. The setTimeout callback function
+const timeoutHandler = () => {
+    console.log("Timeout happens!");
+};
+
+const timeout: number = 1000;
+
+console.log(`// 5. The setTimeout callback function: `);
+
+setTimeout(timeoutHandler, timeout);
+
+// 6. The type of the variable of someValue
+const someValue: number | null = Math.random() > 0.5 ? 12 : null;
+
+
+console.log(`// 6. The type of the variable of someValue: \n`, someValue);
+
+
+// 7. The type of the following object
+
+const peter = {
+    name: "Peter",
+    age: 50,
+    students: [
+        { name: "Andy", age: 20 },
+        { name: "Bob", age: 23 },
+        {
+            name: "Charlie",
+            age: 25,
+            exercises: [{ score: 60, date: new Date("2019-01-05") }],
+        },
+    ],
+};
+
+type peter = {
+    name: string,
+    age: number,
+    student: Object[]
+}
+console.log(peter);
